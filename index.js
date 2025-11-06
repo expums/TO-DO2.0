968444ce473862ae97fe5e7dd7192150beb82a97
expums <expumsoff@gmail.com>
2025-11-07 01:47:38 +0500

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const openModal = document.getElementById("openModal");
+const modal = document.getElementById("noteModal");
+const closeModal = document.getElementById("closeModal");
+
+openModal.onclick = () => modal.style.display = "block";
+closeModal.onclick = () => modal.style.display = "none";
+window.onclick = (e) => {
+    if (e.target === modal) modal.style.display = "none";
+};
